aaea791dbcc7ccd7d0c7db50ce2ea46108915650
hachouuw <hachou@uw.edu>
2022-11-16 15:27:08 -0800

    Update IPCbackbone.py

--- IPCbackbone.py
+++ IPCbackbone.py
@@ -1,6 +1,6 @@
 import zmq
 import time
-
+import numpy as np
 # specify the name of the surface you want to use
 surface_name = "dell_window1"
 
@@ -48,7 +48,19 @@ while time.time() < t_end:
     topic1, payload1 = subscriber1.recv_multipart()
     message0 = msgpack.loads(payload0)
     message1 = msgpack.loads(payload1)
+
+    # diameters for each eye
+    diameter0 = message0[b'diameter']
+    diameter1 = message1[b'diameter']
+
+    # raw pupil positions of each eye
+    raw_pupil0 = message0[b'norm_pos']
+    raw_pupil1 = message1[b'norm_pos'] 
     # raw_x, raw_y = 0.,0.
+
+    # raw_pupil = np.concatenate(np.array((raw_pupil0), dtype = np.float), np.array((raw_pupil1), dtype = np.float)) # convert to np arrays 
+    raw_pupil = np.array((raw_pupil0), dtype = np.float)
+    raw_pupil = raw_pupil[:, np.newaxis].T
 
     # print(f"{topic}: {message}")
     # print('\n pupil 0 diameter:',message0[b'diameter'],'\n pupil 0 confidence:',message0[b'confidence'],'\n pupil 0 time:',message0[b'timestamp'])
@@ -58,6 +70,12 @@ while time.time() < t_end:
     # surfaces = msgpack.loads(msg, raw=False)
 
     # if surfaces["name"] == surface_name:
+
+    # print('\n raw eye0: ', raw_pupil0)
+    # print('\n raw eye1: ', raw_pupil1)
+    # print('\n raw pupil data: ', raw_pupil)
+    print(raw_pupil.shape)
+
     try:
         topic, msg = sub.recv_multipart(flags=zmq.NOBLOCK)
         gaze_position = msgpack.loads(msg, raw=False)   
@@ -70,7 +88,7 @@ while time.time() < t_end:
         raw_x, raw_y = gaze_on_screen[-1]['norm_pos']
     
     except:
-        raw_x, raw_y = 0.,0.
+        raw_x, raw_y = np.nan,np.nan
 
         # else:
         #     raw_x, raw_y = 0.,0.
@@ -80,7 +98,7 @@ while time.time() < t_end:
     #     raw_x, raw_y = 0.,0.
 
     # print('running',time.time())
-    print('\n raw x: ', raw_x)
-    print('\n raw y: ', raw_y)
+    # print('\n raw x: ', raw_x)
+    # print('\n raw y: ', raw_y)
     # print('\n pupil 0 diameter:',message0[b'diameter'],'\n pupil 0 confidence:',message0[b'confidence'],'\n pupil 0 time:',message0[b'timestamp'])
     # print('\n pupil 1 diameter:',message1[b'diameter'],'\n pupil 1 confidence:',message1[b'confidence'],'\n pupil 1 time:',message1[b'timestamp'])
